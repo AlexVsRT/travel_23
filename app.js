34090437a8eb142f7cacfcd618600f927728d9c8
AlexVRt <aleksei.ratt@gmail.com>
2023-10-22 16:49:01 +0500

    Project structure changed.The layout and any_hbs have been created. Routes in app.js have been added. The script for calculating the cost has been moved to a separate file myjs.js.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mysql = require("mysql2"); //подключаем БД
 const express = require("express"); //подключаем фреймворк express
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+
 const app = express(); //создаем объект приложение
 
 const urlencodedParser = express.urlencoded({ extended: false });
@@ -16,6 +19,19 @@ const pool = mysql.createPool({
     database: "travel",     //наша база travel, созданная в прошлой работе
     password: ""            //пароль к нашей базе
 });
+
+// устанавливаем настройки для файлов layout
+app.engine("hbs", expressHbs.engine( 
+    //expressHbs.engine() осуществляет конфигурацию движка
+{
+layoutsDir: "views/layouts", 
+//задает путь к папке с файлами layout относительно корня каталога проекта
+defaultLayout: "layout", 
+//указывает на название файла шаблона
+ extname: "hbs" //задает расширение файлов
+}
+))
+
 //устанавливаем Handlebars в качестве движка представлений в Express
 app.set("view engine", "hbs");
 
@@ -25,16 +41,31 @@ app.set("view engine", "hbs");
 
 // маршурут на главную страницу
 app.get("/", function(req, res){
-    res.render("index.hbs");
+    res.render("index.hbs", {title: "travel"});
 });
 // возвращаем форму для добавления данных
 app.get("/create", function(req, res){
-    res.render("create.hbs");
+    res.render("create.hbs", {title: "Регистрация"});
 });
 // возвращаем браузеру форму для авторизации данных
 app.get("/avtoriz", function (req, res) {
-    res.render("avtoriz.hbs");
+    res.render("avtoriz.hbs", {title: "Авторизация"});
 });
+
+// возвращаем форму с турами
+app.get("/tours", function(req, res){
+    res.render("tours.hbs", {title: "Наши туры"});
+    });
+
+// возвращаем форму о нас
+app.get("/contacts", function(req, res){
+    res.render("contacts.hbs", {title: "О нас"});
+    });
+ 
+// возвращаем форму с отзывами
+app.get("/remarks-all", function(req, res){
+   res.render("remarks-all.hbs", {title: "Отзывы"});
+});    
 
 //получаем отправленные данные со страницы «Регистрация» create.hbs и добавляем их в БД
 app.post("/create", urlencodedParser, function (req, res) {
---
+++ public/js/myjs.js
@@ -0,0 +1,25 @@
+// расчет стоимости в зависимости от месяца
+$(document).ready(function () {
+    $('#orderForm').on('submit', function () {
+        let tour = $("#inp1").val();
+        let date = new Date($("#inp2").val())
+        let month = date.getMonth();
+        let kol = $("#inp3").val()
+        kol = Number(kol)
+        let email = $("#inp4").val();
+        let stoim = 0;
+        if (tour == "Крым") {
+            if ((month > 4) && (month < 9)) { stoim = kol * 500 }
+            else { stoim = kol * 300 }
+        }
+        if (tour == "Кавказ") {
+            if (((month > 4) && (month < 9)) || (month == 12) || (month == 1) || (month == 2)) { stoim = kol * 300 }
+            else { stoim = kol * 250 }
+        }
+        if (tour == 'Алтай') { stoim = kol * 1000 }
+
+        alert(`Примерная стоимость вашего тура на ${kol} человек составит ${stoim} условных едниц. МЫ ВАС НАЙДЕМ!!`);
+    }
+    )
+}
+)
